82d467e7e9353f96d3491169c602a3b9653a10d4
kiruthickraajaa <kiruthickraajaa@gmail.com>
2024-09-23 11:42:53 +0530

    picture

---
+++ java.js
@@ -0,0 +1,13 @@
+function ac()
+{
+   let m = document.getElementById('input').value
+if(m%2==0){
+    k="even"
+    console.log("k")
+}
+else
+k="odd"
+console.log("k")
+}
+let m=document.getElementById('result');
+m.innerHTML= "k";
---
+++ java2.js
@@ -0,0 +1,8 @@
+
+const title = document.getElementById('LAS');
+title.style.color='red';
+console.log(title);
+
+
+const lik =document.getElementsByClassName('DK');
+console.log(lik)
